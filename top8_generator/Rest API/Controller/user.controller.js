84468ced97ae74134c0651c1167ac982ccb2c8d4
AFGRocha <antoniolaranjinha@hotmail.com>
2020-02-06 21:39:39 +0000

    first commit

---
+++ top8_generator/Rest API/Controller/user.controller.js
@@ -0,0 +1,85 @@
+const Canvas = require('canvas')
+const Buffer = require('buffer').Buffer;
+const path = require('path');
+const fs = require('fs');
+
+async function register(req, res) {
+    //console.log(this.second);
+    Canvas.registerFont('./assets/BebasNeue Bold_0.ttf', { family: 'Bebas' })
+    Canvas.registerFont('./assets/Gobold Bold Italic.otf', { family: 'Gobold' })
+    const canvas = Canvas.createCanvas(680, 849);
+    const ctx = canvas.getContext("2d");
+    const background = await Canvas.loadImage('./assets/eubc_imgs/background.png');
+    console.log(background)
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+
+    //Second place
+    ctx.shadowOffsetX = 3;
+    ctx.shadowOffsetY = 3;
+    ctx.shadowColor = "rgba(0,0,0,0.3)";
+    if (req.body.second.team != "") {
+        ctx.font = '35px "Gobold"'
+        ctx.fillStyle = "#f8cf24";
+        ctx.fillText(req.body.second.team, 177, 446);
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(req.body.second.name, ctx.measureText(req.body.second.team).width + 183, 446);
+    }
+    else {
+        ctx.font = '35px "Gobold"'
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText("Daiki", 177, 446);
+    }
+    ctx.shadowOffsetX = 0
+    ctx.shadowOffsetY = 0
+    //stock icons
+    const stock = await Canvas.loadImage(`./assets/stocks/${req.body.second.char1}.png`);
+    ctx.drawImage(stock, 590, 408, 40, 42);
+    if (req.body.second.char2 != "") {
+        const stock2 = await Canvas.loadImage(`./assets/stocks/${req.body.second.char2}.png`);
+        ctx.drawImage(stock2, 547, 408, 40, 42);
+    }
+    if (req.body.second.char3 != "") {
+        const stock3 = await Canvas.loadImage(`./assets/stocks/${req.body.second.char3}.png`);
+        ctx.drawImage(stock3, 504, 408, 40, 42);
+    }
+    if (req.body.second.char4 != "") {
+        const stock4 = await Canvas.loadImage(`./assets/stocks/${req.body.second.char4}.png`);
+        ctx.drawImage(stock4, 461, 408, 40, 42);;
+    }
+
+    //flag
+    const flag = await Canvas.loadImage(`./assets/flags/${req.body.second.country}.png`);
+    ctx.drawImage(flag, 104, 406);
+
+
+
+    //Labels
+    ctx.font = '21px "Bebas"'
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillText(req.body.labels.date, 33, 842);
+    ctx.fillText(`${req.body.labels.entrants} Entrants`, 297, 842);
+    ctx.fillText(req.body.labels.tourney, 648 - ctx.measureText(req.body.labels.tourney).width, 842);
+
+
+    console.log(canvas.toDataURL());
+    let img = canvas.toDataURL();
+    var data = img.replace(/^data:image\/\w+;base64,/, "");
+    var buf = Buffer.from(data, 'base64');
+    console.log(buf);
+    fs.writeFile(path.join(__dirname, `${req.body.labels.tourney}.png`), buf, function (error) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('File created from base64 string!');
+            return true;
+        }
+    });
+    console.log(req.body)
+    console.log("directory: " + __dirname)
+    res.status(201).send("dirname: " + __dirname)
+
+}
+
+
+module.exports = { register }
---
+++ top8_generator/src/views/Home.vue
@@ -0,0 +1,259 @@
+<template>
+  <div class="margin">
+<b-field grouped>
+    <p class="placing">2nd</p>
+       <b-field label="Country">
+            <b-select v-model="second.country">
+                 <option v-for="country in countries" :key="country">{{country}}</option>
+            </b-select>
+          </b-field>
+        <b-field label="Team">
+            <b-input v-model="second.team"></b-input>
+        </b-field>
+        <b-field label="Player">
+            <b-input v-model="second.name"></b-input>
+        </b-field>
+        <b-field label="Character 1">
+            <b-select v-model="second.chars.char1">
+             <option></option>
+             <option v-for="character in characters" :key="character">{{character}}</option>
+            </b-select>
+          </b-field>
+           <b-field label="Character 2">
+            <b-select v-model="second.chars.char2">
+              <option></option>
+              <option v-for="character in characters" :key="character">{{character}}</option>
+            </b-select>
+          </b-field>
+           <b-field label="Character 3">
+            <b-select v-model="second.chars.char3">
+              <option></option>
+             <option v-for="character in characters" :key="character">{{character}}</option>
+            </b-select>
+          </b-field>
+           <b-field label="Character 4">
+            <b-select v-model="second.chars.char4">
+              <option></option>
+                <option v-for="character in characters" :key="character">{{character}}</option>
+            </b-select>
+          </b-field>
+    </b-field>
+<b-field grouped>
+       <p class="placing">Labels</p>
+        <b-field label="Date">
+            <b-input v-model="labels.date"></b-input>
+        </b-field>
+        <b-field label="Entrants">
+            <b-input v-model="labels.entrants"></b-input>
+        </b-field>
+         <b-field label="Tournament">
+            <b-input v-model="labels.tourney"></b-input>
+        </b-field>
+    </b-field>
+
+<center> <button class="button is-primary" @click="createImg">Create</button></center>
+
+  </div>
+</template>
+<style>
+.margin {
+  margin-left: 6%;
+  margin-right: 10%;
+}
+
+.placing {
+  text-align: center;
+  padding-top: 35px;
+  padding-right: 10px;
+}
+</style>
+
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Canvas from "canvas";
+import axios from "axios";
+
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+
+  data: function() {
+    return {
+      second: {
+        country: "",
+        team: "",
+        name: "",
+        chars: { char1: "", char2: "", char3: "", char4: "" }
+      },
+      third: {
+        country: "",
+        team: "",
+        name: "",
+        chars: { char1: "", char2: "", char3: "", char4: "" }
+      },
+      forth: {
+        country: "",
+        team: "",
+        name: "",
+        chars: { char1: "", char2: "", char3: "", char4: "" }
+      },
+      fith: {
+        country: "",
+        team: "",
+        name: "",
+        chars: { char1: "", char2: "", char3: "", char4: "" }
+      },
+      fith2: {
+        country: "",
+        team: "",
+        name: "",
+        chars: { char1: "", char2: "", char3: "", char4: "" }
+      },
+      seventh: {
+        country: "",
+        team: "",
+        name: "",
+        chars: { char1: "", char2: "", char3: "", char4: "" }
+      },
+      seventh2: {
+        country: "",
+        team: "",
+        name: "",
+        chars: { char1: "", char2: "", char3: "", char4: "" }
+      },
+      labels:{
+        date: "",
+        entrants: "",
+        tourney: ""
+      },
+      characters: [
+        "Banjo & Kazooie",
+        "Bayonetta",
+        "Bowser",
+        "Bowser Jr.",
+        "Byleth",
+        "Captain Falcon",
+        "Chrom",
+        "Cloud",
+        "Corrin",
+        "Daisy",
+        "Dark Pit",
+        "Dark Samus",
+        "Diddy Kong",
+        "Donkey Kong",
+        "Dr. Mario",
+        "Duck Hunt",
+        "Falco",
+        "Fox",
+        "Ganondorf",
+        "Greninja",
+        "Hero",
+        "Ice Climbers",
+        "Ike",
+        "Incineroar",
+        "Inkling",
+        "Isabelle",
+        "Ivysaur",
+        "Jigglypuff",
+        "Joker",
+        "Ken",
+        "King Dedede",
+        "King K. Rool",
+        "Kirby",
+        "Link",
+        "Little Mac",
+        "Lucario",
+        "Lucas",
+        "Lucina",
+        "Luigi",
+        "Mario",
+        "Marth",
+        "Mega Man",
+        "Meta Knight",
+        "Mewtwo",
+        "Mii Brawler",
+        "Mii Swordfighter",
+        "Mii Gunner",
+        "Mr. Game & Watch",
+        "Ness",
+        "Olimar",
+        "Pac-Man",
+        "Palutena",
+        "Peach",
+        "Pichu",
+        "Pikachu",
+        "Piranha Plant",
+        "Pit",
+        "Pokemon Trainer",
+        "Richter",
+        "Ridley",
+        "R.O.B.",
+        "Robin",
+        "Rosalina & Luma",
+        "Roy",
+        "Ryu",
+        "Samus",
+        "Sheik",
+        "Shulk",
+        "Simon",
+        "Snake",
+        "Sonic",
+        "Squirtle",
+        "Terry",
+        "Toon Link",
+        "Villager",
+        "Wario",
+        "Wii Fit Trainer",
+        "Wolf",
+        "Yoshi",
+        "Young Link",
+        "Zelda",
+        "Zero Suit Samus"
+      ],
+      countries: [
+        "Austria",
+        "Belgium",
+        "Denmark",
+        "Finland",
+        "France",
+        "Germany",
+        "Greece",
+        "Ireland",
+        "Italy",
+        "Netherlands",
+        "Norway",
+        "Portugal",
+        "Spain",
+        "Sweden",
+        "United Kingdom"
+      ]
+    };
+  },
+  methods: {
+    async createImg() {
+      console.log(this.second);
+      axios.post("http://localhost:3000/users", {
+        second:{
+            country: this.second.country,
+            team: this.second.team,
+            name: this.second.name,
+            char1: this.second.chars.char1,
+            char2: this.second.chars.char2,
+            char3: this.second.chars.char3,
+            char4: this.second.chars.char4
+        },
+        labels:{
+          date: this.labels.date,
+          entrants: this.labels.entrants,
+          tourney: this.labels.tourney
+        }
+        });
+    }
+  }
+};
+</script>
