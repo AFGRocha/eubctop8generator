5ed8751b8150d7cbd9f4e8addae99966c3898689
António Rocha <afgrocha98@gmail.com>
2020-11-12 03:46:27 +0000

    changing project to be on react instead

---
+++ top8-generator/src/Row.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Row extends React.Component {
+    render() {
+        return (<div>
+
+            <label>Country</label>
+            <select>
+                <option >teste</option>
+                {/*<option >{{ country }}</option> */}
+            </select>
+            <label>Team</label>
+            <input type="text" />
+            <label>Player</label>
+            <input type="text" />
+            <label>Character 1</label>
+            <select>
+                <option>teste</option>
+                {/*<option >{{ character }}</option> */}
+            </select>
+            <label>Color</label>
+            <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+            </select>
+            <label>Character 2</label>
+            <select>
+                <option>teste</option>
+                {/*<option >{{ character }}</option> */}
+            </select>
+            <label>Character 3</label>
+            <select>
+                <option>teste</option>
+                {/*<option >{{ character }}</option> */}
+            </select>
+            
+        </div>);
+    }
+}
+
+export default Row;
